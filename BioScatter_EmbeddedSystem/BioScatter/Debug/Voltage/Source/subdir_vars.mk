62305edc1291626794a436092a1ac6cad0e13c9b
Wenli Jiao <31982814+wlJiao@users.noreply.github.com>
2023-04-12 17:17:02 +0800

    Add files via upload

---
+++ BioScatter_EmbeddedSystem/BioScatter/Debug/Voltage/Source/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Voltage/Source/voltage.c 
+
+C_DEPS += \
+./Voltage/Source/voltage.d 
+
+OBJS += \
+./Voltage/Source/voltage.obj 
+
+OBJS__QUOTED += \
+"Voltage\Source\voltage.obj" 
+
+C_DEPS__QUOTED += \
+"Voltage\Source\voltage.d" 
+
+C_SRCS__QUOTED += \
+"../Voltage/Source/voltage.c" 
+
+
