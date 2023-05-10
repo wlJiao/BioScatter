ec605a3ac36048468d416489a0c4c125b3bd9737
Anonymous <31982814+wlJiao@users.noreply.github.com>
2023-05-10 20:53:11 +0800

    Add files via upload

---
+++ BioScatter_EmbeddedSystem/BioScatter/Debug/Communication/Source/communication.d
@@ -0,0 +1,39 @@
+# FIXED
+
+Communication/Source/communication.obj: ../Communication/Source/communication.c
+Communication/Source/communication.obj: D:/LYL/Desktop/BioScatter/BioScatter/Communication/Include/communication.h
+Communication/Source/communication.obj: C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h
+Communication/Source/communication.obj: C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/msp430fr2355.h
+Communication/Source/communication.obj: C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/in430.h
+Communication/Source/communication.obj: C:/ProgramFiles/Ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+Communication/Source/communication.obj: C:/ProgramFiles/Ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+Communication/Source/communication.obj: C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/legacy.h
+Communication/Source/communication.obj: D:/LYL/Desktop/BioScatter/BioScatter/System/Include/system.h
+Communication/Source/communication.obj: D:/LYL/Desktop/BioScatter/BioScatter/sysconf.h
+Communication/Source/communication.obj: D:/LYL/Desktop/BioScatter/BioScatter/System/Include/sysdef.h
+Communication/Source/communication.obj: D:/LYL/Desktop/BioScatter/BioScatter/System/board.h
+
+../Communication/Source/communication.c:
+
+D:/LYL/Desktop/BioScatter/BioScatter/Communication/Include/communication.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/legacy.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/System/Include/system.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/sysconf.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/System/Include/sysdef.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/System/board.h:
+
---
+++ BioScatter_EmbeddedSystem/BioScatter/Debug/main.d
@@ -0,0 +1,45 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/msp430fr2355.h
+main.obj: C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/ProgramFiles/Ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+main.obj: C:/ProgramFiles/Ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/legacy.h
+main.obj: D:/LYL/Desktop/BioScatter/BioScatter/System/Include/system.h
+main.obj: D:/LYL/Desktop/BioScatter/BioScatter/sysconf.h
+main.obj: D:/LYL/Desktop/BioScatter/BioScatter/System/Include/sysdef.h
+main.obj: D:/LYL/Desktop/BioScatter/BioScatter/System/board.h
+main.obj: D:/LYL/Desktop/BioScatter/BioScatter/Sensor/Include/sensor.h
+main.obj: D:/LYL/Desktop/BioScatter/BioScatter/Communication/Include/communication.h
+main.obj: D:/LYL/Desktop/BioScatter/BioScatter/Voltage/Include/voltage.h
+
+../main.c:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/msp430fr2355.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ProgramFiles/Ti/ccs1210/ccs/ccs_base/msp430/include/legacy.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/System/Include/system.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/sysconf.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/System/Include/sysdef.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/System/board.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/Sensor/Include/sensor.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/Communication/Include/communication.h:
+
+D:/LYL/Desktop/BioScatter/BioScatter/Voltage/Include/voltage.h:
+
